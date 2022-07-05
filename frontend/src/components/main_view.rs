3c23f8870ad52eb9550415db92201a33575d6211
sheepy0125 <sheepy404@gmail.com>
2022-07-05 15:59:47 -0400

    Rename clipboard_get to clipboard_image

--- frontend/src/components/main_view.rs
+++ frontend/src/components/main_view.rs
@@ -6,13 +6,13 @@
 /***** Setup *****/
 /* Imports */
 use yew::prelude::*;
-#[path="./clipboard_get.rs"]
-mod clipboard_get;
+#[path = "./clipboard_image.rs"]
+mod clipboard_image;
 
 /***** Main view *****/
 #[function_component(MainView)]
 pub fn main_view() -> Html {
     html! {
-        <clipboard_get::ClipboardReader />
+        <clipboard_image::ClipboardReader />
     }
 }
