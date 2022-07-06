216e56a71cb14be9d428fb9a41b5619235e5ea55
sheepy0125 <sheepy404@gmail.com>
2022-07-06 15:01:53 -0400

    Right side is collapsible

--- frontend/src/components/main_view.rs
+++ frontend/src/components/main_view.rs
@@ -16,12 +16,28 @@ mod settings;
 /***** Main view *****/
 #[function_component(MainView)]
 pub fn main_view() -> Html {
+    // Right side collapsed
+    let right_side_collapsed = use_state_eq(|| true);
+    let on_collapsed_toggle_click = {
+        let right_side_collapsed = right_side_collapsed.clone();
+        Callback::from(move |_| right_side_collapsed.set(!*right_side_collapsed))
+    };
     html! {
         <div class="flex p-2 min-h-screen">
-            <div class="flex-1 mr-2">
+            // Image view
+            <div class="flex-1">
                 <clipboard_image::ClipboardImage />
             </div>
-            <div class="flex flex-col flex-initial">
+            // Settings and controls
+            <div class={
+                format!(
+                    "ml-2 flex-col flex-initial {}",
+                    match *right_side_collapsed {
+                        false => "hidden",
+                        true => "flex"
+                    }
+                )
+            }>
                 <div class="flex-1 mb-2">
                     <settings::Settings />
                 </div>
@@ -29,6 +45,15 @@ pub fn main_view() -> Html {
                     <controls::Controls />
                 </div>
             </div>
+            // Collapse
+            <button onclick={on_collapsed_toggle_click} class="ml-2 text-white">
+                {
+                    match *right_side_collapsed {
+                        true => ">",
+                        false => "<"
+                    }
+                }
+            </button>
         </div>
     }
 }
